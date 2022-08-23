62a38d76fd0e33ce74f16de4592f20cbaa497966
vicenteyah <vicente.yah.dzul@icloud.com>
2022-08-23 18:15:12 -0500

    Home structure added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,19 @@
-import { Link } from "react-router-dom";
-
+import ResponsiveAppBar from './../components/organisms/Home/Header/Navigation';
+import Section from '../components/organisms/Home/Content/SectionContent';
+import { Grid } from '@mui/material'
 export const Home = () => {
 
   return (
-    <nav>
-      <Link to="/login">LogIn</Link>
-    </nav>
+    <>
+      <ResponsiveAppBar/>
+      <Section>
+        <Grid item>
+          <h1>Home</h1>
+        </Grid>
+        <Grid item>
+          <h1>Home2</h1>
+        </Grid>
+      </Section>
+    </>
   );
 }
